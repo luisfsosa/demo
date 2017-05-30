44e68c49509c9adcb6a8f788164d9ccb1dae9ff0
Luis Felipe Sosa <luisfsosa@gmail.com>
2017-05-30 08:46:14 -0500

    Se arregla las pestañas en angular

---
+++ src/main/resources/static/js/app.config.js
@@ -0,0 +1,29 @@
+angular.module('hello')
+	.config([ '$routeProvider', '$httpProvider', '$locationProvider',
+		function($routeProvider, $httpProvider, $locationProvider) {
+
+			$locationProvider.html5Mode(true);
+
+			$routeProvider.when('/', {
+				templateUrl : 'js/home/home.html',
+				controller : 'HomeController',
+				controllerAs : 'controller'
+			}).when('/message', {
+				templateUrl : 'js/message/message.html',
+				controller : 'MessageController',
+				controllerAs : 'controller'
+			}).when('/login', {
+				templateUrl : 'js/navigation/login.html',
+				controller : 'NavigationController',
+				controllerAs : 'controller'
+			}).otherwise('/');
+
+			$httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+		} ]).run(function(auth) {
+
+	// Initialize auth module with the home page and
+	// login/logout path
+	// respectively
+	auth.init('/', '/login', '/logout');
+});
